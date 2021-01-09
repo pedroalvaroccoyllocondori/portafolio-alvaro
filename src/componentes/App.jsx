9739c4f94569eaa809ad4eecf01badaf4d801431
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2021-01-09 12:54:26 -0500

    creacion de estudios ,presentacion , proyectos

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import BannerMenu from './moleculas/BannerMenu'
-import Contacto from './moleculas/Contacto'
+// import Contacto from './moleculas/Contacto'
+import Presentacion from "./moleculas/Presentacion"
+
 
 
 const App=()=>(
   <>
   <BannerMenu/>
-  <Contacto/>
+  <Presentacion/>
+  
   </>
 )
 
---
+++ src/componentes/moleculas/Presentacion.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import "../../estilos/estilosPresentacion.css"
+
+
+
+const Presentacion=()=>(
+    <>
+    <div className="contenedor">
+        <div className="titulo">about</div>
+        <div className="contenedor-descripccion">
+            <div className="item">
+            <h1>hello</h1>
+            <p>my name is pedro alvaro im a developer sofware a love programing </p>
+            </div>
+           
+        </div>
+    </div>
+    </>
+)
+
+export default Presentacion
