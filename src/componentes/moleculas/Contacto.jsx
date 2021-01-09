7a5d0bb069ea47341ca9dcde1f3defd8ddf14ba3
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2021-01-09 11:06:25 -0500

    poniendo estilos a componente contacto

--- src/componentes/moleculas/Contacto.jsx
+++ src/componentes/moleculas/Contacto.jsx
@@ -7,12 +7,12 @@ const Contacto=()=>(
     <div className="contenedor">
         <div className="titulo">contact</div>
         <div className="contenedor-imagenes">
-            <div className="item">Email</div>
-            <div className="item">GitHub</div>
-            <div className="item">GitLab</div>
-            <div className="item">Facebook</div>
-            <div className="item">Whatsapp</div>
-            <div className="item">linklink</div>
+            <div className="item"><p>pedroalvaroccoyllocondori</p><img src="https://img.icons8.com/color/2x/gitlab.png"/></div>
+            <div className="item"><p>alvaro.ccoyllocondori</p><img src="https://www.flaticon.es/svg/static/icons/svg/1312/1312139.svg"/></div>
+            <div className="item"><p>pedroalvaroccoyllocondori</p><img src="https://www.flaticon.es/svg/static/icons/svg/733/733609.svg"/></div>
+            <div className="item"><p>+51 917186613</p><img src="https://www.flaticon.com/svg/static/icons/svg/733/733585.svg"/></div>
+            <div className="item"><p>Pedro Alvaro</p><img src="https://www.flaticon.es/svg/static/icons/svg/174/174857.svg"/></div>
+            <div className="item">San idelfonso Ica-Peru<img src="https://www.flaticon.es/svg/static/icons/svg/684/684908.svg"/></div>
         </div>
     </div>
     </>
