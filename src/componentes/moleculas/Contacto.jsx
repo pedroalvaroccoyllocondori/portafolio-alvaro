0d0e305e563f556da655a034797ca92acc7fbc0c
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2021-04-07 22:35:05 -0500

    ultima modificacion caja ica

--- src/componentes/moleculas/Contacto.jsx
+++ src/componentes/moleculas/Contacto.jsx
@@ -11,8 +11,8 @@ const Contacto=()=>(
             <div className="item"><p>alvaro.ccoyllocondori</p><img src="https://www.flaticon.es/svg/static/icons/svg/1312/1312139.svg"/></div>
             <div className="item"><p>pedroalvaroccoyllocondori</p><img src="https://www.flaticon.es/svg/static/icons/svg/733/733609.svg"/></div>
             <div className="item"><p>+51 917186613</p><img src="https://www.flaticon.com/svg/static/icons/svg/733/733585.svg"/></div>
-            <div className="item"><p>Pedro Alvaro</p><img src="https://www.flaticon.es/svg/static/icons/svg/174/174857.svg"/></div>
-            <div className="item">San idelfonso Ica-Peru<img src="https://www.flaticon.es/svg/static/icons/svg/684/684908.svg"/></div>
+            <div className="item"><p>alvaroccoyllo</p><img src="https://image.flaticon.com/icons/png/512/2111/2111463.png"/></div>
+            <div className="item">San idelfonso Mz17 lt 01 Ica-Peru<img src="https://www.flaticon.es/svg/static/icons/svg/684/684908.svg"/></div>
         </div>
     </div>
     </>
--- src/componentes/moleculas/Presentacion.jsx
+++ src/componentes/moleculas/Presentacion.jsx
@@ -9,9 +9,13 @@ const Presentacion=()=>(
         <div className="titulo">about</div>
         <div className="contenedor-descripccion">
             <div className="item">
-            <p className="item-titulo">hello</p>
-            <div className="item-imagen"></div>
-            <p className="item-descripcion">my name is pedro alvaro im a developer sofware a love programing </p>
+            <p className="item-titulo">Hola</p>
+            <div className="item-imagen">
+                <img src="https://scontent.flim14-1.fna.fbcdn.net/v/t1.6435-0/s206x206/161666276_3803253236394683_7351810753727348620_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=da31f3&_nc_ohc=lpDmsDT-cWcAX-QM_N2&_nc_ht=scontent.flim14-1.fna&tp=7&oh=ff4c09ad28427186e47bdd47b4904f9f&oe=60936571"></img>
+            </div>
+            <p className="item-descripcion">Mi Nombre es pedro alvaro soy programador,desarollador web y diseñador ux/ui.
+            Me gusta mucho crear nuevas aplicaciones , trabajar en equipo y aprender nuevas tecnologias.
+            Todo ello con el fin de resolver problemas </p>
             </div>
            
         </div>
