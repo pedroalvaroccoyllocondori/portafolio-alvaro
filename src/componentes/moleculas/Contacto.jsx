27ccc7687e80cc30257cc263e922079f2468e081
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2021-04-07 22:44:58 -0500

    modificacion 2

--- src/componentes/moleculas/Contacto.jsx
+++ src/componentes/moleculas/Contacto.jsx
@@ -7,12 +7,12 @@ const Contacto=()=>(
     <div className="contenedor">
         <div className="titulo">contact</div>
         <div className="contenedor-imagenes">
-            <div className="item"><p>pedroalvaroccoyllocondori</p><img src="https://img.icons8.com/color/2x/gitlab.png"/></div>
-            <div className="item"><p>alvaro.ccoyllocondori</p><img src="https://www.flaticon.es/svg/static/icons/svg/1312/1312139.svg"/></div>
-            <div className="item"><p>pedroalvaroccoyllocondori</p><img src="https://www.flaticon.es/svg/static/icons/svg/733/733609.svg"/></div>
-            <div className="item"><p>+51 917186613</p><img src="https://www.flaticon.com/svg/static/icons/svg/733/733585.svg"/></div>
-            <div className="item"><p>alvaroccoyllo</p><img src="https://image.flaticon.com/icons/png/512/2111/2111463.png"/></div>
-            <div className="item">San idelfonso Mz17 lt 01 Ica-Peru<img src="https://www.flaticon.es/svg/static/icons/svg/684/684908.svg"/></div>
+            <div className="item"><p>pedroalvaroccoyllocondori</p><img alt="imagen1" src="https://img.icons8.com/color/2x/gitlab.png"/></div>
+            <div className="item"><p>alvaro.ccoyllocondori</p><img alt="imagen1" src="https://www.flaticon.es/svg/static/icons/svg/1312/1312139.svg"/></div>
+            <div className="item"><p>pedroalvaroccoyllocondori</p><img alt="imagen1" src="https://www.flaticon.es/svg/static/icons/svg/733/733609.svg"/></div>
+            <div className="item"><p>+51 917186613</p><img  alt="imagen1" src="https://www.flaticon.com/svg/static/icons/svg/733/733585.svg"/></div>
+            <div className="item"><p>alvaroccoyllo</p><img  alt="imagen1" src="https://image.flaticon.com/icons/png/512/2111/2111463.png"/></div>
+            <div className="item">San idelfonso Mz17 lt 01 Ica-Peru<img alt="imagen1" src="https://www.flaticon.es/svg/static/icons/svg/684/684908.svg"/></div>
         </div>
     </div>
     </>
