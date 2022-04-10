dc5bb448ec253055a93cee0c2a45e8684b08972d
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-04-10 12:13:54 -0500

    modificaciones1

--- src/componentes/moleculas/BannerMenu.jsx
+++ src/componentes/moleculas/BannerMenu.jsx
@@ -4,20 +4,25 @@ import "../../estilos/estilosBannerMenu.scss"
 
 const BannerMenu =()=>(
     <>
-    <div className="contenedor-banner">
-      <di className="titulo-banner">ALVARO CCOYLLO</di>
-      <div className="contenedor-menu">
-        <div className="item-menu contenedor-izquierda"></div>
-        <div className="contenedor-derecha">
+      <div className="menu">
             <div className="item-menu"><NavLink exact activeClassName="estadoActivo" className="item-link" to="/">Acerca</NavLink></div>
             <div className="item-menu"><NavLink activeClassName="estadoActivo"  className="item-link" to="/proyectos">proyectos</NavLink></div>
             <div className="item-menu"><NavLink activeClassName="estadoActivo"  className="item-link" to="/contacto">contacto</NavLink></div>
             <div className="item-menu"><NavLink activeClassName="estadoActivo"  className="item-link" to="/estudios">estudios</NavLink></div>
         </div>
-     
-    </div>
+    <div className="contenedor-banner">
+        <div className="TITULO">
+            <div className="CONTENEDOR_TITULO">
+                <p>PEDRO ALVARO</p>
+                <p>CCOYLLO CONDORI</p>
+                <p>PROGRAMADOR Y DISEÑADOR WEB  </p>
+            </div>   
+        </div>
+
+      
+
     </div>
     </>
 )
-
+        
 export default BannerMenu
